03d568ee0ff99c036b107f62816d9dfe15bea6ef
Andrew Agenbroad <andrew.agenbroad@ucdenver.edu>
2015-09-07 18:12:08 -0600

    First commit of completed PA1

---
+++ Point.h
@@ -0,0 +1,36 @@
+
+//A 3-dimensional point class!
+// Coordinates are double-precision floating point.
+
+#ifndef __point_h
+#define __point_h
+
+class Point {
+
+private:
+    double x;
+    double y;
+    double z;       //new private member z (third point)
+public:
+    // Constructors
+    Point();                      // default constructor
+    Point(double x, double y, double z);    // three argument constructor updated from the original two argument
+
+    // Destructor
+    ~Point();
+
+    // Mutator methods
+    void setX(double newX);
+    void setY(double newY);
+    void setZ(double newZ);         //prototype for new mutator method for point z
+
+    // Accessor methods
+    double getX();
+    double getY();
+    double getZ();                  //prototype for new accessor method for point z
+
+    double distanceTo(Point &);   //new function to find the distance between two points
+};
+
+#endif // __point_h
+
